25ac15987f3cdde6438e973f86883afe3332b962
Sebastian Bauer <48291831+git-bauerseb@users.noreply.github.com>
2023-05-24 20:28:10 +0200

    Added preprocessing utility for own extrinsics/intrinsics

--- scripts/preprocess/process_raw_to_nerfstudio.py
+++ scripts/preprocess/process_raw_to_nerfstudio.py
@@ -31,7 +31,7 @@ def get_transform_matrix_idx(jsonObj, index):
     into separate location. The images may be downscaled and information about
     the scene is stored in a JSON file which can be processed by nerfstudio.
 """
-def main(
+def nerfstudio_to_masked(
         rgb_path: str,
         transform_path: str,
         out_path: str,
@@ -40,6 +40,9 @@ def main(
         down_scale: int = 1
 ):
     """
+        Transform a given dataset with extrinsics/intrinsics provided in transform.json into another representation,
+        possibly with mask supervision.
+
         Arguments:
             rgb_path        - Path to RGB images.
             transform_path  - Path to transforms.json file that contains camera extrinsic/intrinsic parameters.
@@ -76,10 +79,10 @@ def main(
 
     # Read intrinsic parameters
     outJson = dict()
-    outJson['fl_x'] = tJSON['fl_x']
-    outJson['fl_y'] = tJSON['fl_y']
-    outJson['cx'] = int(tJSON['cx'] / down_scale)
-    outJson['cy'] = int(tJSON['cy'] / down_scale)
+    outJson['fl_x'] = tJSON['fl_x'] / down_scale
+    outJson['fl_y'] = tJSON['fl_y'] / down_scale
+    outJson['cx'] = tJSON['cx'] / down_scale
+    outJson['cy'] = tJSON['cy'] / down_scale
     outJson['w'] = int(tJSON['w'] / down_scale)
     outJson['h'] = int(tJSON['h'] / down_scale)
     outJson['k1'] = tJSON['k1']
@@ -90,9 +93,13 @@ def main(
 
     print('Output size: Width: {}, Height: {}'.format(outJson['w'], outJson['h']))
 
+    print(len(tJSON['frames']))
+
     for file in arr:
         # If file is image
         if file.endswith('.png') or file.endswith('.jpg'):
+
+            print('INDEX: {}'.format(idx))
 
             IN_FILE = '/'.join([rgb_path, file])
             OUT_FILE = '/'.join([OUT_IMG_PATH, file])
@@ -125,9 +132,12 @@ def main(
 
                 assert nWidth == outJson['w'] and nHeight == outJson['h']
                 img = Image.fromarray(img).resize((nWidth, nHeight))
-                iio.v3.imwrite(OUT_FILE, img)
+                blackWhite = img.convert('1')
 
-                frameInfo['mask_path'] = OUT_FILE
+                with open(OUT_FILE[:-4] + '.jpg', 'w') as maskImg:
+                    blackWhite.save(maskImg)
+
+                frameInfo['mask_path'] = OUT_FILE[:-4] + '.jpg'
 
             outJson['frames'].append(frameInfo)
             idx += 1
@@ -136,19 +146,135 @@ def main(
     with open('/'.join([out_path, 'transforms.json']), 'w') as tFile:
         json.dump(outJson, tFile, ensure_ascii=False, indent=4, cls=NumpyEncoder)
 
+def own_to_masked(
+        rgbPath: str,
+        maskPath: str,
+        outPath: str,
+        extrFile: str,
+        intrFile: str,
+        useMask: bool = True,
+        downScale: float = 1.0
+):
+    extrinsics = np.load(extrFile, allow_pickle=True)[()]
+    intrinsics = np.load(intrFile, allow_pickle=True)[()]
+
+    transforms = dict()
+
+    # Assume no distortion
+    transforms['k1'] = 0
+    transforms['k2'] = 0
+    transforms['p1'] = 0
+    transforms['p2'] = 0
+
+    # Original width, height
+    oWidth, oHeight = 0,0
+
+    # Infer the width/height of an image
+    for key in extrinsics:
+        RGB_FILE = '/'.join([rgbPath, key + '.png'])
+        img = iio.v3.imread(RGB_FILE)
+        oWidth, oHeight = img.shape[1], img.shape[0]
+        break
+
+    # Get intrinsics
+    # As they are shared, use intrinsic of first camera
+    for key in intrinsics:
+        fx, fy, cx, cy = intrinsics[key][0][0], intrinsics[key][1][1], intrinsics[key][0][2], intrinsics[key][1][2]
+        transforms['fl_x'] = float(fx/downScale)
+        transforms['fl_y'] = float(fy/downScale)
+        transforms['cx'] = float(cx/downScale)
+        transforms['cy'] = float(cy/downScale)
+        break
+
+    # New width, height
+    nWidth, nHeight = int(oWidth / downScale), int(oHeight / downScale)
+
+    transforms['w'] = nWidth
+    transforms['h'] = nHeight
+    transforms['camera_model'] = 'OPENCV'
+
+    transforms['frames'] = []
+
+    OUT_RGB = '/'.join([outPath, 'images'])
+    OUT_MASK = '/'.join([outPath, 'masks'])
+
+    for dir in [OUT_RGB, OUT_MASK]:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+
+    for key in extrinsics:
+        # Write mask + image
+        IN_FILE = '/'.join([rgbPath, key + '.png'])
+        OUT_FILE = '/'.join([OUT_RGB, key + '.png'])
+
+        img = iio.v3.imread(IN_FILE)
+        img = Image.fromarray(img).resize((nWidth, nHeight))
+        iio.v3.imwrite(OUT_FILE, img)
+
+
+        frameInfo = dict()
+        frameInfo['file_path'] = OUT_FILE
+
+        mat = np.array(extrinsics[key])
+
+        # Extrinsics is here in OPENCV format
+        # Convert to OPENGL
+        print(mat)
+        mat[1,:] *= -1
+        mat[2,:] *= -1
+        print(mat)
+
+        frameInfo['transform_matrix'] = np.array(extrinsics[key])
+
+        # Process mask
+        if useMask:
+            IN_FILE = '/'.join([maskPath, key + '.png'])
+            OUT_FILE = '/'.join([OUT_MASK, key + '.png'])
+            img = iio.v3.imread(IN_FILE)
+            img = Image.fromarray(img).resize((nWidth, nHeight))
+            blackWhite = img.convert('1')
+            with open(OUT_FILE[:-4] + '.jpg', 'w') as maskImg:
+                blackWhite.save(maskImg)
+            frameInfo['mask_path'] = OUT_FILE[:-4] + '.jpg'
+
+        transforms['frames'].append(frameInfo)
+
+    with open('/'.join([outPath, 'transforms.json']), 'w') as tFile:
+        json.dump(transforms, tFile, ensure_ascii=False, indent=4, cls=NumpyEncoder)
+
 
 if __name__ == '__main__':
-    RGB_PATH = '/mnt/hdd/data/head_01'
-    TRANSFORM_PATH = '/mnt/hdd/data/head_01_nerfstudio'
+    '''
+    TRANSFORM_PATH = '/mnt/hdd/data/head_38_nerfstudio'
 
-    MASK_PATH = '/mnt/hdd/data/head_01/masks'
-    OUT_PATH = '/mnt/hdd/data/head_01_own'
+    RGB_PATH = '/mnt/hdd/data/head_38/frame_00000/images'
+    MASK_PATH = '/mnt/hdd/data/head_38/frame_00000/masks'
 
-    main(
+    OUT_PATH = '/mnt/hdd/data/head_38_own'
+
+    nerfstudio_to_masked(
         RGB_PATH,
         TRANSFORM_PATH,
         OUT_PATH,
         MASK_PATH,
         True,
-        down_scale=2
+        down_scale=4
+    )
+    '''
+
+    RGB_PATH = '/mnt/hdd/data/head_38/frame_00000/images'
+    MASK_PATH = '/mnt/hdd/data/head_38/frame_00000/masks'
+
+    OUT_PATH = '/mnt/hdd/data/head_38_own'
+    EXTR_FILE = '/mnt/hdd/data/head_38/extrinsics.npy'
+    INTR_FILE = '/mnt/hdd/data/head_38/intrinsics.npy'
+
+    own_to_masked(
+        RGB_PATH,
+        MASK_PATH,
+        OUT_PATH,
+        EXTR_FILE,
+        INTR_FILE,
+        True,
+        4.0
     )
